c2675ddfd9ba976a9d567ed083c49cedcd1f2d94
Iurii <ikariyuu@gmail.com>
2022-04-24 10:04:16 +0200

    Init branch room-2

--- src/main/java/com/codecool/sprinter3000/UserStoryController.java
+++ src/main/java/com/codecool/sprinter3000/UserStoryController.java
@@ -2,6 +2,8 @@ package com.codecool.sprinter3000;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserStoryController {
@@ -10,5 +12,11 @@ public class UserStoryController {
     @Autowired
     public UserStoryController(UserStoryService userStoryService) {
         this.userStoryService = userStoryService;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        //model.addAttribute("notes", noteService.getAll());
+        return "index";
     }
 }
