5e6cfb1b5627991a5bef39cb491c2d9c6d1429ca
Iurii <ikariyuu@gmail.com>
2022-04-24 09:30:24 +0200

    Init commit

---
+++ src/main/java/com/codecool/sprinter3000/Sprinter3000Application.java
@@ -0,0 +1,13 @@
+package com.codecool.sprinter3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprinter3000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprinter3000Application.class, args);
+	}
+
+}
